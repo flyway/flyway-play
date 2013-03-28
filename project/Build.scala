78210f407feb08a8b2c881078ee755e0d8e82f78
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2013-03-28 23:22:24 +0900

    Added settings for publishing

--- project/Build.scala
+++ project/Build.scala
@@ -11,6 +11,7 @@ object ApplicationBuild extends Build {
     settings = Defaults.defaultSettings ++ Seq (
       name := "play-flyway",
       organization := "com.github.tototoshi",
+      version := "0.1.0",
       scalaVersion := "2.10.0",
       libraryDependencies ++= Seq(
         "play" %% "play" % "2.1.0" % "provided",
@@ -18,7 +19,7 @@ object ApplicationBuild extends Build {
         "commons-io" % "commons-io" % "2.4"
       ),
       scalacOptions ++= Seq("-language:_", "-deprecation")
-    ) ++ scalariformSettings
+    ) ++ scalariformSettings ++ publishingSettings
 
   )
 
@@ -47,5 +48,39 @@ object ApplicationBuild extends Build {
     )
       .dependsOn(plugin)
       .aggregate(plugin)
+
+  val publishingSettings = Seq(
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) => _publishTo(v) },
+    publishArtifact in Test := false,
+    pomExtra := _pomExtra
+  )
+
+  def _publishTo(v: String) = {
+    val nexus = "https://oss.sonatype.org/"
+    if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots")
+    else Some("releases" at nexus + "service/local/staging/deploy/maven2")
+  }
+
+  val _pomExtra =
+    <url>http://github.com/tototoshi/play-flyway</url>
+    <licenses>
+      <license>
+        <name>Two-clause BSD-style license</name>
+        <url>http://github.com/tototoshi/play-flyway/blob/master/LICENSE.txt</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+    <scm>
+      <url>git@github.com:tototoshi/play-flyway.git</url>
+      <connection>scm:git:git@github.com:tototoshi/play-flyway.git</connection>
+    </scm>
+    <developers>
+      <developer>
+        <id>tototoshi</id>
+        <name>Toshiyuki Takahashi</name>
+        <url>http://tototoshi.github.com</url>
+      </developer>
+    </developers>
 
 }

c1696fadae32bb57ddc7aad5a19476f303f78b36
Daniel Mijares <daniel.j.mijares@gmail.com>
2016-01-10 21:22:57 -0500

    FlywayPlayComponents for compile time DI

---
+++ plugin/src/main/scala/org/flywaydb/play/FlywayPlayComponents.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2013 Toshiyuki Takahashi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flywaydb.play
+
+import play.api._
+import play.core._
+
+trait FlywayPlayComponents {
+  def configuration: Configuration
+  def environment: Environment
+  def webCommands: WebCommands
+
+  lazy val playInitializer = new PlayInitializer(configuration, environment, webCommands)
+}

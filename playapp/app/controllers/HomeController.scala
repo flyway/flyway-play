d5204e2c60ada9efdd434a5ed8000718e71bce18
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2017-06-25 11:36:47 +0900

    Fixed compile-time DI example

--- playapp/app/controllers/HomeController.scala
+++ playapp/app/controllers/HomeController.scala
@@ -5,7 +5,8 @@ import javax.inject.{ Inject, Singleton }
 import play.api.mvc._
 
 @Singleton
-class Application @Inject() () extends InjectedController {
+class HomeController @Inject() (controllerComponents: ControllerComponents)
+    extends AbstractController(controllerComponents) {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
--- playapp/app/loader/MyApplicationLoader.scala
+++ playapp/app/loader/MyApplicationLoader.scala
@@ -2,19 +2,21 @@ package loader
 
 import org.flywaydb.play.FlywayPlayComponents
 import play.api._
+import _root_.controllers.HomeController
 import play.api.ApplicationLoader.Context
+import play.filters.HttpFiltersComponents
 
 class MyApplicationLoader extends ApplicationLoader {
-  def load(context: Context) = {
+  def load(context: Context): Application = {
     new MyComponents(context).application
   }
 }
 
 class MyComponents(context: Context) extends BuiltInComponentsFromContext(context)
     with FlywayPlayComponents
-    with NoHttpFiltersComponents
+    with HttpFiltersComponents
     with _root_.controllers.AssetsComponents {
   flywayPlayInitializer
-  lazy val applicationController = new _root_.controllers.Application()
+  lazy val applicationController = new HomeController(controllerComponents)
   lazy val router = new _root_.router.Routes(httpErrorHandler, applicationController, assets)
 }
